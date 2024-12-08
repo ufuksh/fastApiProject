cfc60598ef5a2631302dab939e6d8b2891385edd
ufuksh <439504@ogr.ktu.edu.tr>
2024-12-08 19:53:29 +0300

    last

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,14 @@
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-
-# Modellerden Base sınıfını içe aktarın
-import sys
 import os
+import sys
 
+# Proje kök dizinini Python path'ine ekleyin
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-from app.models import Base  # Base'in doğru şekilde import edildiğinden emin olun
+
+# Modelleri içe aktarın
+from app.models import Base
 
 # Alembic Config nesnesi
 config = context.config
